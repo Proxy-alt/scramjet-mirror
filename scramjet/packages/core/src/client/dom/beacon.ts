1b4c7ae3ea41b2407f2bcac6c7bb47ff0ac2d8a8
Proxy Alt <proxy-alt@proxy-alt.dev>
2025-12-01 09:26:35 -0500

    Delete scramjet directory

--- scramjet/packages/core/src/client/dom/beacon.ts
+++
@@ -1,9 +0,0 @@
-import { ScramjetClient } from "@client/index";
-
-export default function (client: ScramjetClient, _self: Self) {
-	client.Proxy("Navigator.prototype.sendBeacon", {
-		apply(ctx) {
-			ctx.args[0] = client.rewriteUrl(ctx.args[0]);
-		},
-	});
-}
